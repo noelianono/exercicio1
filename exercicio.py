cdb72828c6f99f8fa596eb84e6b0b6067a1bb09c
noelianono <noelia19731@gmail.com>
2024-11-18 11:51:10 -0300

    primeiro teste

---
+++ exercicio.py
@@ -0,0 +1,7 @@
+#Exercício 1.2.
+
+print("Extremamente básico\n")
+A = input("Digite o primeiro número inteiro: ")
+B = input("Digite o segundo número inteiro: ")
+X = int(A) + int(B)
+print(f"X =  {X}")
