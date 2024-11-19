af0e3b58ffb393051fed749615cf4e5aa5f517c8
noelianono <noelia19731@gmail.com>
2024-11-19 17:17:29 -0300

    exerci-5

---
+++ diferença.py
@@ -0,0 +1,11 @@
+def main():
+    a = int(input('A '))
+    b = int(input('B '))
+    c = int(input('C '))
+    D = int(input('D '))
+    mult = a * b 
+    mult1 = c * D
+    diferença = mult - mult1
+
+    print(f'resultado de {diferença} ')
+main()
---
+++ exerc-função.py
@@ -0,0 +1,21 @@
+
+def calcular_e_exibir():
+    fun = int(input('salario '))
+    h = int(input('horas '))
+    n = int(input('Numero = '))
+    s = fun / 30  
+    d = 30 / 8
+    e = d * h
+
+
+    print(f"\n\n{n:>13}{'Numero = ':>26}{n:>2}")
+    print(f"{s:14.2f}\n{e:14.2f}{'$':>18} {fun:2.2f}\n{d:14.2f}")
+    
+
+
+
+
+calcular_e_exibir()
+
+
+
